fc57dd08ee97535cb133dd097bb3004c6f25ecd0
Ludwig <ludwig.nachname.spam@gmail.com>
2021-01-19 16:35:35 +0100

    Renamed and moved file

--- wikipedia_scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://en.wikipedia.org/wiki/The_Great_British_Bake_Off_(series_1)'
-
-page = requests.get(url)
-
-soup = BeautifulSoup(page.content)
