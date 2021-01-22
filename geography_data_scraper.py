cde1a32bb33aa93db4d035fe8abc7a979d12adaf
Ludwig <ludwig.nachname.spam@gmail.com>
2021-01-22 18:36:58 +0100

    Implemented image scraping to parse_soup()

--- geography_data_scraper.py
+++ geography_data_scraper.py
@@ -10,19 +10,93 @@ from bs4 import BeautifulSoup
 def parse_soup(soup, feature_list):
     """Parse wikipedia page."""
 
+    # get infobox, search and extract image sources
     infobox = soup.find('table', attrs={'class': 'infobox'})
-    tablerow_list = infobox.find_all('tr')
-    # TODO: hier auch die Flagge rausziehen
+    image_list = infobox.find_all('a', {'class': 'image'})
+    parsed_images = pd.DataFrame(columns=['category', 'feature', 'value'])
+    for image_idx, image in enumerate(image_list):
+        image_page = requests.get(
+            'https://commons.wikimedia.org/' + image.get('href')).text
+        image_soup = BeautifulSoup(image_page, 'lxml')
+        image_source = image_soup.find_all(
+            'a', href=True, text='Original file')
+        parsed_images.loc[image_idx] = [
+            'Image', image.get('title'), image_source[0].get('href')]
 
-    parsed_data = {}
-    for tablerow in tablerow_list:
-        try:
+    # search and extract text from table rows
+    tablerow_list = infobox.find_all('tr')
+    parsed_text = pd.DataFrame(columns=['category', 'feature', 'value'])
+    for tablerow_idx, tablerow in enumerate(tablerow_list):
+        parsed_text.loc[tablerow_idx] = [
+            get_category(tablerow), get_feature(tablerow), get_value(tablerow)]
+
+    parsed_data = parsed_images.append(parsed_text, ignore_index=True)
+    return parsed_data
+
+
+def get_category(tablerow):
+    """..."""
+    if tablerow.get('class'):
+        if tablerow.get('class')[0] == 'mergedtoprow':
+            category = get_feature(tablerow)
+        else:
+            category = get_category(tablerow.previous_sibling)
+    else:
+        category = get_feature(tablerow)
+    return category
+
+
+def get_feature(tablerow):
+    """..."""
+    tableheader = tablerow.find('th')
+    if tableheader:
+        feature = tableheader.text
+    else:
+        feature = 'None'
+    return feature
+
+
+def get_value(tablerow):
+    """..."""
+    tabledata = tablerow.find('td')
+    if tabledata:
+        value = tabledata.text
+    else:
+        value = 'None'
+    return value
+
+
+"""         try:
             feature = uc.normalize('NFKC', tablerow.th.text)
             feature = clean_str(feature)
         except AttributeError:
-            continue
+            continue """
 
-        if feature in feature_list.values:
+
+def clean_str(raw_string):
+    """Clean data from unnecessary stuff."""
+
+    # TODO: make this method
+
+    # lower case, [ bis ] wegnehmen, GEO UEI oder so, diese Punkte
+    cleaned_string = re.sub('[^a-zA-Z0-9()]', '', raw_string)
+    # _nicht löschen
+    # [auch nicht löschen]
+    # next step: immer von ( bis ) löschenauch bei []
+    # lower case einführen
+
+    """ get_feature(tablerow)
+        get_value(tablerow)
+        tableheader = tablerow.find('th')
+        if tableheader:
+            tabledata = tablerow.find('td')
+            if tabledata:
+                parsed_data.loc[tablerow_idx] = [
+                    get_category(tablerow), tableheader.text, tabledata.text]
+            else:
+                parsed_data.loc[tablerow_idx] = [
+                    get_category(tablerow), tableheader.text, 'None'] """
+    """         if feature in feature_list.values:
             try:
                 parsed_data[feature] = uc.normalize('NFKC', tablerow.td.text)
             except AttributeError:
@@ -30,45 +104,9 @@ def parse_soup(soup, feature_list):
                 while sibling.get('class')[0] != 'mergedbottomrow':
                     sibling = sibling.next_sibling
                     parsed_data[feature + uc.normalize('NFKC', sibling.th.text)] = uc.normalize(
-                        'NFKC', sibling.td.text)
-
-    # TODO: man kann nicht die try exept methode nehmen, weil da manchmal was drinsteht
-    # also doch etwas mit area_total einführen
-
-    # TODO: eine contains Methode einführen, damit largest city und capital beide erkannst werden,
-    # aufpassen weil man dann eigentlich noch mal an den anfang springen müsste
-
-    # TODO: Bild saugen aktualisieren
-
-    # TODO: clean str methode fertig machen
-        # if 'Constructors' in attr:
-        #    parsed_data['Constructor titles'] = tr.td.text.split()[0]
-        #    continue
-
-        # if 'Drivers' in attr:
-        #    parsed_data['Driver titles'] = tr.td.text.split()[0]
-        #    continue
-    return parsed_data
-
-
-def clean_str(raw_string):
-    # lower case, [ bis ] wegnehmen, GEO UEI oder so, diese Punkte
-    cleaned_string = re.sub('[^a-zA-Z0-9()', '', raw_string)
-    # _nicht löschen
-    # [auch nicht löschen]
-    # next step: immer von ( bis ) löschenauch bei []
-    # lower case einführen
+                        'NFKC', sibling.td.text) """
 
     return cleaned_string
-
-
-def sanitize(data_dict):
-    """Clean data from unnecessary stuff."""
-    res = data_dict.copy()
-
-    # Remove wiki references in square brackets.
-    # for k, v in res.items():
-    return res
 
 
 def fetch_num(st):
@@ -88,18 +126,15 @@ def make_dataset_row(data_dict):
 
 def main():
     url_list = pd.read_csv('url_list.csv', header=None)
-    feature_list = pd.read_csv('tablerow_list.csv', header=None)
+    feature_list = pd.read_csv('feature_list.csv', header=None)
     data = []
     for url in url_list.values:
         print("\n* Parsing data from {0}".format(url))
         page = requests.get(url[0]).text
         soup = BeautifulSoup(page, 'lxml')
         parsed_data = parse_soup(soup, feature_list)
-        # bis hier eig auf bel. tabellen anwendbar
 
-        # ab hier dann sehr individuell
-        # vllt datatype in tablerow_list einführen, um das cleanen einfacher zu machen
-        # dann in feature list umbenennen
+        # TODO: Hier weiter
 
         if parsed_data:
             parsed_data = sanitize(parsed_data)
