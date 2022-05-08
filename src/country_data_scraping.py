45db88e718ed0afb1a547466e0a7902386453de9
Ludwig <ludwig.nachname.spam@gmail.com>
2022-05-08 16:45:02 +0200

    Debugged search_routine()

--- src/country_data_scraping.py
+++ src/country_data_scraping.py
@@ -6,9 +6,6 @@ import cleaners.string_cleaner as sc
 import scrapers.static_website_scraper as sws
 
 # TODO: search routine testen mit test urls und dabei die phrases anpassen
-# gerade bei map
-# funktioniert für afghanistan so gerade nicht gut
-# ggfs besser, wenn man erst nach href sucht und dann erst nach title
 
 
 def get_states_list():
@@ -162,25 +159,6 @@ def get_state_attributes(link, attributes):
     return state_attributes
 
 
-def search_routine(series, phrases, index_start=0, index_stop=1):
-
-    match = [s for s in series if all(
-        sp in s for sp in phrases[index_start:index_stop])]
-
-    if len(match) == 1:
-        warnings.warn(
-            'no distinct match found.')
-        return match
-    elif index_start == len(phrases)-1 or index_stop == len(phrases):
-        return []
-    elif len(match) > 1:
-        match = search_routine(series, phrases, index_start, index_stop+1)
-    elif len(match) == 0:
-        match = search_routine(series, phrases, index_start+1, index_start+2)
-
-    return match
-
-
 def get_state_flag(link):
     """Scrape an url for the flag of a state from Wikipedia.
 
@@ -202,10 +180,23 @@ def get_state_flag(link):
         url=link,
         link_attributes={'class': 'image'})
 
-    # search for link to the flag and check validity
-    search_phrases = ['Flag', link.rsplit('/', 1)[-1]]
+    # search for link to the flag by title
+    search_phrases = ['Flag', link.rsplit('/', 1)[-1].replace('_', ' ')]
     flag_match = search_routine(
-        scraped_links.iloc[:, 0], search_phrases)
+        scraped_links['title'].astype(str), search_phrases)
+
+    # get href of flag match
+    if len(flag_match) == 1:
+        flag_match = scraped_links['href'][scraped_links['title']
+                                           == flag_match[0]].to_list()
+
+    # search for link to the flag by href
+    else:
+        search_phrases = ['Flag', link.rsplit('/', 1)[-1]]
+        flag_match = search_routine(scraped_links.iloc[:, 0], search_phrases)
+        if len(flag_match) == 0:
+            raise ValueError(
+                'no match found for the flag of:' + link)
 
     # scrape follow up links of an image to get the original
     scraped_links = sws.scrape_links(
@@ -259,7 +250,7 @@ def get_state_map(link):
 
     # search for link to the map by href
     else:
-        search_phrases = ['orthographic', 'Location', link.rsplit('/', 1)[-1]]
+        search_phrases = ['orthographic', link.rsplit('/', 1)[-1], 'Location']
         map_match = search_routine(
             scraped_links['href'], search_phrases)
         if len(map_match) == 0:
@@ -285,19 +276,49 @@ def get_state_map(link):
     return state_map
 
 
+def search_routine(series, phrases, index_start=0, index_stop=1):
+    """Search a series of strings for an orderes list of sub-strings.
+
+    Parameters:
+        series (pd.Series): strings to search in
+        phrases (list): strings to search for
+        index_start (int): index to select sub-strings from phrases
+        index_stop (int): index to select sub-strings from phrases
+
+    Returns:
+        match (list): unique search result
+
+    Raises:
+        Warning: no distinct match found when searching
+    """
+    match = [s for s in series if all(
+        sp in s for sp in phrases[index_start:index_stop])]
+
+    if len(match) == 1:
+        return match
+    elif index_start == len(phrases)-1 or index_stop == len(phrases):
+        return []
+    elif len(match) > 1:
+        match = search_routine(series, phrases, index_start, index_stop+1)
+    elif len(match) == 0:
+        match = search_routine(series, phrases, index_start+1, index_start+2)
+
+    return match
+
+
 def test_some_url(url):
     attributes_list = get_attributes_list()
     state_dict = {'link': url}
     # state_dict.update(get_state_attributes(
     #    state_dict['link'], attributes_list))
-    # state_dict.update(get_state_flag(state_dict['link']))
+    state_dict.update(get_state_flag(state_dict['link']))
     state_dict.update(get_state_map(state_dict['link']))
     pass
 
 
 def main():
-    test_url = 'https://en.wikipedia.org/wiki/Afghanistan'
-    test_some_url(test_url)
+    #test_url = 'https://en.wikipedia.org//wiki/Bahrain'
+    # test_some_url(test_url)
 
     # init dict to collect dicts of individual states
     states_dict = {}
